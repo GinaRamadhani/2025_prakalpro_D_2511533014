0544ae72a2567ca46a3a5021dc2f0313169443f5
Acer <Acer@Rara>
2025-09-17 15:13:49 +0700

    pekan1

---
+++ prakalpro_2025_D_2511533014/src/pekan1/HelloWord.java
@@ -0,0 +1,21 @@
+package pekan1;
+
+public class HelloWord{
+   public static void main (String [] args) {
+	//program pertama
+	/*komentar
+	 *  dua baris atau lebih
+	 *  pertemuan 1 praktikum alpro
+	 */
+	System.out.println("Hello Word");
+	System.out.println();
+	System.out.println("program ini menghasilkan");
+	System.out.println("Empat baris output");
+	System.out.println("/ \\ // \\\\ /// \\\\");
+	System.out.println("This"+"program prints aln"
+			+ "qoute from the gettyburg address.");
+
+	  
+
+}
+}
