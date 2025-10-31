7e8380cce1fdc62b43e2f1c822c4256fb8653818
GinaRamadhani <ramadhanigina12@gmail.com>
2025-11-01 00:36:56 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511533014/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,22 @@
+package pekan2;
+
+public class tugasAlproPekan2 {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data dasar
+        int umur = 19;
+        float ipk = 3.75f;
+        char kelas = 'B';
+        boolean aktif = true;
+
+        // Output ke layar
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.println("Umur   : " + umur);
+        System.out.println("IPK    : " + ipk);
+        System.out.println("Kelas  : " + kelas);
+        System.out.println("Aktif  : " + aktif);
+    }
+}
+
+
+
+
