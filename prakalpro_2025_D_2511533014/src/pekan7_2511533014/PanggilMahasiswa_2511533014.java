95fa1d35305134b42ca760a1d886ad0a902b8a51
GinaRamadhani <ramadhanigina12@gmail.com>
2025-11-12 15:22:04 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511533014/src/pekan7_2511533014/PanggilMahasiswa_2511533014.java
@@ -0,0 +1,14 @@
+package pekan7_2511533014;
+
+public class PanggilMahasiswa_2511533014 {
+	public static void main(String[] args) {
+		Mahasiswa_2511533014 a= new Mahasiswa_2511533014();
+		a.setNim(23532);
+		a.setNama ("Gina");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+		
+	}
+
+}
